38ebaea8927d640d4f648a4b30b405208ca53014
louis <louisbamidele@icloud.com>
2024-05-03 04:52:45 -0500

    nav

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -19,7 +19,7 @@ function Nav() {
         </p>
         <p className='p-2 bg-cyan-500 rounded cursor-pointer'>
           {" "}
-          <Link href='/login'>Admin login</Link>
+          <Link href='/login'>Login</Link>
         </p>
       </div>
     </div>
