b2d4655b65bd2e2a69273aa0988c0c9bc85fcfef
louis <louisbamidele@icloud.com>
2024-05-05 17:07:57 -0500

    add icon for empty course

--- app/admin_portal/page.tsx
+++ app/admin_portal/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Image from "next/image";
 import Link from "next/link";
+import empty from "@/public/empty-box.png";
 import { MdOutlineDelete } from "react-icons/md";
 import { useEffect, useState } from "react";
 export default function Home() {
@@ -26,29 +27,47 @@ export default function Home() {
 
   return (
     <div className='min-h-screen p-5 overflow-auto'>
-      <table className='w-full overflow-auto mr-10'>
-        <tr>
-          <th>course title</th>
-          <th>course code</th>
-          <th>unit</th>
-          <th>level</th>
-          <th>lecturer</th>
-        </tr>
-        {courses.map((course: any, index: number) => (
-          <tr key={index} className='relative'>
-            <td>{course.coursetitle}</td>
-            <td>{course.coursecode}</td>
-            <td>{course.unit}</td>
-            <td>{course.level}</td>
-            <td>{course.lecturer}</td>
-            <div
-              onClick={() => handleDelete(course._id)}
-              className='absolute text-red-500 cursor-pointer top-[10%] left-[102%] text-3xl flex items-center justify-center'>
-              <MdOutlineDelete />
-            </div>
+      {courses.length > 0 ? (
+        <table className='w-full overflow-auto mr-10'>
+          <tr>
+            <th>course title</th>
+            <th>course code</th>
+            <th>unit</th>
+            <th>level</th>
+            <th>lecturer</th>
           </tr>
-        ))}
-      </table>
+          {courses.map((course: any, index: number) => (
+            <tr key={index} className='relative'>
+              <td>{course.coursetitle}</td>
+              <td>{course.coursecode}</td>
+              <td>{course.unit}</td>
+              <td>{course.level}</td>
+              <td>{course.lecturer}</td>
+              <div
+                onClick={() => handleDelete(course._id)}
+                className='absolute text-red-500 cursor-pointer top-[10%] left-[102%] text-3xl flex items-center justify-center'>
+                <MdOutlineDelete />
+              </div>
+            </tr>
+          ))}
+        </table>
+      ) : (
+        <div className='flex flex-col justify-between items-center gap-5'>
+          <h3>
+            You have no available courses please go to{" "}
+            <Link className='text-cyan-500' href='/admin_portal/new_course'>
+              + Add new course
+            </Link>{" "}
+            to add new courses that would be displayed here
+          </h3>
+          <Image
+            src={empty}
+            alt='empty box indicating empty courses'
+            width={200}
+            height={200}
+          />
+        </div>
+      )}
     </div>
   );
 }
